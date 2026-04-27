1cbc3734578581c3bae7b6c78d3dc9482f841d0f
Ferdy Fauzi <payen83@gmail.com>
2026-04-27 17:12:01 +0800

    add contacts

--- routes/contacts/contacts_routes.js
+++ routes/contacts/contacts_routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const contacts = [
+var contacts = [
     {id: 1, name: "Khairul Ikhwan", phone: "0198878293"},
     {id: 2, name: "Zafrul Noordin", phone: "0176663838"},
     {id: 3, name: "Wong Siew Lan", phone: "0123393939" }
@@ -13,6 +13,30 @@ router.get('/', (req, res) => {
         content: 'Manage and view details',
         contacts
     });
+});
+
+router.get('/add', (req, res) => {
+    renderFormPage(res);
+});
+
+function renderFormPage(res, error = null){
+    res.render('contact/contact_form', {
+        title: 'Add New Contact',
+        content: 'Fill in the details',
+        error,
+        formAction: '/contacts/add'
+    });
+}
+
+router.post('/add', (req,res) => {
+    const {name, phone} = req.body;
+    const newContact = {
+        id: contacts.length+1,
+        name,
+        phone
+    }
+    contacts.push(newContact);
+    res.redirect('/contacts');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send('Hello Express!');
